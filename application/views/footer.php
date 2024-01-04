9f575fe01428200364bad862c0d578ef467c91de
hyoga999 <yogaharimas@gamil.com>
2024-01-04 11:07:19 +0700

    tanggal max kirim po 040124

--- application/views/footer.php
+++ application/views/footer.php
@@ -104,6 +104,68 @@
     return c
   }
 
+  function  tanggal_format_indonesia(tgl)
+	{
+      tanggal    = tgl.split('-');
+      bulan      = getBulan(parseInt(tanggal[1]));
+      tahun      = tanggal[0];
+      return  tanggal[2]+' '+bulan+' '+tahun;
+  }
+
+  function getBulan(bln)
+	{
+      switch  (bln)
+      {
+        case  1:
+          return  "Januari";
+        break;
+
+        case  2:
+          return  "Februari";
+        break;
+
+        case  3:
+          return  "Maret";
+        break;
+
+        case  4:
+          return  "April";
+        break;
+
+        case  5:
+          return  "Mei";
+        break;
+
+        case  6:
+          return  "Juni";
+        break;
+
+        case  7:
+          return  "Juli";
+        break;
+
+        case  8:
+          return  "Agustus";
+        break;
+
+        case  9:
+          return  "September";
+        break;
+
+        case  10:
+          return  "Oktober";
+        break;
+
+        case  11:
+          return  "November";
+        break;
+
+        case  12:
+          return  "Desember";
+        break;
+      }
+    }
+
   function show_loading()
   {
     $('#loading').show();
