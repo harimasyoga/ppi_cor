253f6c09beb99125df32380f6516c38c40096fb1
cuksmokey <bolongs360@gmail.com>
2024-01-17 18:34:21 +0700

    Edit Laporan Pengiriman. 170124

--- application/views/Laporan/v_lap_pengiriman.php
+++ application/views/Laporan/v_lap_pengiriman.php
@@ -8,7 +8,34 @@
 	</section>
 	<section class="content">
 		<div class="container-fluid">
-			LAPORAN PENGIRIMAN
+			<div class="row">
+				<div class="col-md-2">
+					<div class="card card-secondary card-outline">
+						<div class="card-header" style="padding:12px">
+							<h3 class="card-title" style="font-weight:bold;font-size:18px">TANGGAL</h3>
+						</div>
+						<div class="card-body row" style="padding:6px 7px">
+							<div class="col-md-12">
+								<input type="date" id="tgl" class="form-control" onchange="pilihTanggal()">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-10">
+					<div class="card card-info card-outline">
+						<div class="card-header" style="padding:12px">
+							<h3 class="card-title" style="font-weight:bold;font-size:18px">LIST LAPORAN PENGIRIMAN <span class="span-tanggal"></span></h3>
+						</div>
+						<div class="card-body row" style="padding:6px 7px">
+							<div class="col-md-12">
+								<div id="list-laporan">
+									PILIH TANGGAL
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 </div>
@@ -17,6 +44,29 @@
 	$(document).ready(function() {
 		$(".select2").select2()
 	});
-	
 
+	function pilihTanggal(){
+		let tgl = $("#tgl").val()
+		$.ajax({
+			url: '<?php echo base_url('Laporan/plhTglLapPengiriman')?>',
+			type: "POST",
+			data: ({ tgl }),
+			beforeSend: function() {
+				swal({
+					title: 'Loading',
+					allowEscapeKey: false,
+					allowOutsideClick: false,
+					onOpen: () => {
+						swal.showLoading();
+					}
+				});
+			},
+			success: function(res){
+				data = JSON.parse(res)
+				$(".span-tanggal").html(data.tgl)
+				$("#list-laporan").html(data.html)
+				swal.close()
+			}
+		})
+	}
 </script>
